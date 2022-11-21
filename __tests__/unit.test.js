0cce77854f082f452fae7a8e906f2adc4ba93cd6
Charles Young <cjyoung2001@yahoo.com>
2022-11-20 19:12:22 -0800

    finished first 4 tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,24 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('isPhoneNumbertrue1', () => {
+    let phone = '408-888-8888';
+    expect(functions.isPhoneNumber(phone)).toBe(true);
+})
+
+test('isPhoneNumbertrue2', () => {
+    let phone = '408-594-1234';
+    expect(functions.isPhoneNumber(phone)).toBe(true);
+})
+
+test('isPhoneNumberfalse1', () => {
+    let phone = 'we ball';
+    expect(functions.isPhoneNumber(phone)).toBe(false);
+})
+
+test('isPhoneNumberfalse2', () => {
+    let phone = '(408)555-132';
+    expect(functions.isPhoneNumber(phone)).toBe(false);
+})
+
+
